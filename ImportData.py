576fb58bb38bd321fe6b1d77809348a7524b8b4e
sble <44573600+sbleh@users.noreply.github.com>
2018-11-12 11:57:35 -0500

    Update ImportData.py

--- ImportData.py
+++ ImportData.py
@@ -1,21 +1,34 @@
 import os
-# import numpy as np
-# import nibabel as nib
+import numpy as np
+import nibabel as nib
 
-data_directory = "C:/Users/steff/Desktop/LunaProject/data"
+def import_data(data_directory):
+    # imports data from directory and returns numpy arrays input and labels with size [512, 512, num_slices]
 
-counter = 0
-files = os.listdir(data_directory)
+    files = os.listdir(data_directory)
+    inputSize = len(files)
+    inputShape = [512, 512]
 
-inputSize = len(files)
-inputShape = [512, 512]
+    input = np.zeros((inputShape[0], inputShape[1], 0))
+    labels = np.zeros((inputShape[0], inputShape[1], 0))
 
-for filename in files:
-    directory = data_directory + filename
-    #currentFile = nib.load(directory)
-    directory = directory[:-7]
-    directory = directory + "-label.nii.gz"
-    # currentLabel = nib.load(directory)
-    counter = counter +1
+    print("Start importing data, Progress:")
+    counter = 0
+    for filename in files:
+        directory = data_directory + filename
+        currentFile = nib.load(directory).get_fdata()
+        if directory.find("label") < 0:
+            input = np.concatenate((input, currentFile), axis=2)
+        else:
+            labels = np.concatenate((labels, currentFile), axis=2)
+        counter = counter+1
+        print(counter/inputSize)
 
-print(input)
+    print("Import done, Input Size:")
+    print(input.shape)
+
+    return [input, labels]
+
+
+data_directory = "C:/Users/steff/Desktop/LunaProject/data-cropped/"
+input, labels = import_data(data_directory)
