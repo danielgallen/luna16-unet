92a2eeaaaf845c3226c2c053c044b2ed18a2117a
sble <44573600+sbleh@users.noreply.github.com>
2018-11-12 13:17:09 -0500

    added import, builtmodel

---
+++ built_network.py
@@ -0,0 +1,118 @@
+from importingdata import import_data
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib.keras import layers
+from tensorflow.contrib.keras import models
+from tensorflow.python.keras import losses
+import matplotlib.pyplot as plt
+
+def conv_block(input_tensor, num_filters):
+    encoder = layers.Conv2D(num_filters, (3, 3), padding='same')(input_tensor)
+    encoder = layers.BatchNormalization()(encoder)
+    encoder = layers.Activation('relu')(encoder)
+    encoder = layers.Conv2D(num_filters, (3, 3), padding='same')(encoder)
+    encoder = layers.BatchNormalization()(encoder)
+    encoder = layers.Activation('relu')(encoder)
+    return encoder
+
+
+def encoder_block(input_tensor, num_filters):
+    encoder = conv_block(input_tensor, num_filters)
+    encoder_pool = layers.MaxPooling2D((2, 2), strides=(2, 2))(encoder)
+    return encoder_pool, encoder
+
+
+def decoder_block(input_tensor, concat_tensor, num_filters):
+    decoder = layers.Conv2DTranspose(num_filters, (2, 2), strides=(2, 2), padding='same')(input_tensor)
+    decoder = layers.concatenate([concat_tensor, decoder], axis=-1)
+    decoder = layers.BatchNormalization()(decoder)
+    decoder = layers.Activation('relu')(decoder)
+    decoder = layers.Conv2D(num_filters, (3, 3), padding='same')(decoder)
+    decoder = layers.BatchNormalization()(decoder)
+    decoder = layers.Activation('relu')(decoder)
+    decoder = layers.Conv2D(num_filters, (3, 3), padding='same')(decoder)
+    decoder = layers.BatchNormalization()(decoder)
+    decoder = layers.Activation('relu')(decoder)
+    return decoder
+
+def create_model(img_shape):
+    inputs = layers.Input(shape=img_shape)
+    encoder0_pool, encoder0 = encoder_block(inputs, 32)
+    encoder1_pool, encoder1 = encoder_block(encoder0_pool, 64)
+    encoder2_pool, encoder2 = encoder_block(encoder1_pool, 128)
+    encoder3_pool, encoder3 = encoder_block(encoder2_pool, 256)
+    encoder4_pool, encoder4 = encoder_block(encoder3_pool, 512)
+    center = conv_block(encoder4_pool, 1024)
+    decoder4 = decoder_block(center, encoder4, 512)
+    decoder3 = decoder_block(decoder4, encoder3, 256)
+    decoder2 = decoder_block(decoder3, encoder2, 128)
+    decoder1 = decoder_block(decoder2, encoder1, 64)
+    decoder0 = decoder_block(decoder1, encoder0, 32)
+    outputs = layers.Conv2D(1, (1, 1), activation='sigmoid')(decoder0)
+    model = models.Model(inputs=[inputs], outputs=[outputs])
+    return model
+
+def dice_coeff(y_true, y_pred):
+    smooth = 1.
+    # Flatten
+    y_true_f = tf.reshape(y_true, [-1])
+    y_pred_f = tf.reshape(y_pred, [-1])
+    intersection = tf.reduce_sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth)
+    return score
+
+def dice_loss(y_true, y_pred):
+    loss = 1 - dice_coeff(y_true, y_pred)
+    return loss
+
+def bce_dice_loss(y_true, y_pred):
+    loss = losses.binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+    return loss
+
+# get data
+directoryOfFiles = "C:/Users/steff/Desktop/LunaProject/data-cropped/"
+input, labels = import_data(directoryOfFiles)
+
+# split data in training and validation
+train_ds =
+val_ds =
+
+
+# create the model
+img_shape = (512, 512, 1)
+num_train_examples = train_ds.shape[2]
+num_val_examples = val_ds.shape[2]
+batch_size = 10
+epochs = 700
+
+model = create_model(img_shape)
+model.compile(optimizer='adam', loss=bce_dice_loss, metrics=[dice_loss])
+
+history = model.fit(train_ds,
+                   steps_per_epoch=int(np.ceil(num_train_examples / float(batch_size))),
+                   epochs=epochs,
+                   validation_data=val_ds,
+                   validation_steps=int(np.ceil(num_val_examples / float(batch_size))),)
+
+dice = history.history['dice_loss']
+val_dice = history.history['val_dice_loss']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(16, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, dice, label='Training Dice Loss')
+plt.plot(epochs_range, val_dice, label='Validation Dice Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Dice Loss')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+
+plt.show()
---
+++ importingdata.py
@@ -0,0 +1,35 @@
+import os
+import numpy as np
+import nibabel as nib
+
+
+def import_data(data_directory):
+    # imports data from directory and returns numpy arrays input and labels with size [512, 512, num_slices]
+
+    files = os.listdir(data_directory)
+    input_size = len(files)
+    input_shape = [512, 512]
+
+    input_matrix = np.zeros((input_shape[0], input_shape[1], 0))
+    labels_matrix = np.zeros((input_shape[0], input_shape[1], 0))
+
+    print("Start importing data, Progress:")
+    counter = 0
+    for filename in files:
+        directory = data_directory + filename
+        if directory.find("normalized") >= 0:
+            current_file = nib.load(directory).get_fdata()
+            input_matrix = np.concatenate((input_matrix, current_file), axis=2)
+        if directory.find("label") >= 0:
+            current_file = nib.load(directory).get_fdata()
+            labels_matrix = np.concatenate((labels_matrix, current_file), axis=2)
+        counter = counter + 1
+        print(counter / input_size)
+
+    print("Import done, Input Size:")
+    print(input_matrix.shape)
+    print(labels_matrix.shape)
+
+    return [input_matrix, labels_matrix]
+
+
