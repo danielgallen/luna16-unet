aa948a7e6ba066aa8c99aa5a5e67f1b104e69528
sble <44573600+sbleh@users.noreply.github.com>
2018-11-16 16:34:21 -0500

    cxcn

--- test_loading.py
+++ test_loading.py
@@ -11,8 +11,9 @@ from sklearn.model_selection import train_test_split
 # Change to relative path
 directoryOfFiles = "./data/train/"
 img_shape = (512, 512, 1)
-#batch_size = 2
-epochs = 10 
+# batch_size = 2
+epochs = 10
+
 
 # functions for creating a unet model
 def conv_block(input_tensor, num_filters):
@@ -44,6 +45,7 @@ def decoder_block(input_tensor, concat_tensor, num_filters):
     decoder = layers.Activation('relu')(decoder)
     return decoder
 
+
 # create the unet
 def create_model(img_shape):
     inputs = layers.Input(shape=img_shape)
@@ -62,6 +64,7 @@ def create_model(img_shape):
     model = models.Model(inputs=[inputs], outputs=[outputs])
     return model
 
+
 # define loss function
 def dice_coeff(y_true, y_pred):
     smooth = 1.
@@ -72,24 +75,28 @@ def dice_coeff(y_true, y_pred):
     score = (2. * intersection + smooth) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth)
     return score
 
+
 def dice_loss(y_true, y_pred):
     loss = 1 - dice_coeff(y_true, y_pred)
     return loss
+
 
 def bce_dice_loss(y_true, y_pred):
     loss = losses.binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
     return loss
 
+
 def generator(features, labels):
-    batch_size=3
+    batch_size = 3
     batch_features = np.zeros((batch_size, 512, 512, 1))
     batch_labels = np.zeros((batch_size, 512, 512, 1))
     while True:
         for i in range(batch_size):
-            #index = random.choice(len(features),1)
+            # index = random.choice(len(features),1)
             batch_features[i] = features[i]
             batch_labels[i] = labels[i]
         yield batch_features, batch_labels
+
 
 # get data
 input, labels = import_data(directoryOfFiles)
@@ -101,10 +108,6 @@ print("Prepare data for training")
 input = np.moveaxis(input, -1, 0)
 labels = np.moveaxis(labels, -1, 0)
 x_train, x_val, y_train, y_val = train_test_split(input, labels, test_size=0.1)
-# x_train = np.swapaxes(x_train, 0, 3)
-# x_val = np.swapaxes(x_val, 0, 3)
-# y_train = np.swapaxes(y_train, 0, 3)
-# y_val = np.swapaxes(y_val, 0, 3)
 
 # create the model
 print("Creating the model")
@@ -115,31 +118,32 @@ model = create_model(img_shape)
 
 # Train the model
 print("Start training")
-#save_model_path = "../temp/weights.hdf5"
+save_model_path = "../temp/weights.hdf5"
 model.compile(optimizer='adam', loss=dice_loss, metrics=[dice_loss])
 model.summary()
-#cp = tf.contrib.keras.callbacks.ModelCheckpoint(filepath=save_model_path, monitor='val_dice_loss', save_best_only=True, verbose=1)
+cp = tf.contrib.keras.callbacks.ModelCheckpoint(filepath=save_model_path, monitor='dice_loss', save_best_only=True,
+                                                verbose=1)
 
-history = model.fit_generator(generator(x_train, y_train), epochs=epochs, steps_per_epoch=20)
+history = model.fit_generator(generator(x_train, y_train), epochs=epochs, steps_per_epoch=20, callback=[cp])
 
 dice = history.history['dice_loss']
-#val_dice = history.history['val_dice_loss']
+# val_dice = history.history['val_dice_loss']
 
 loss = history.history['loss']
-#val_loss = history.history['val_loss']
+# val_loss = history.history['val_loss']
 
 epochs_range = range(epochs)
 
 plt.figure(figsize=(16, 8))
 plt.subplot(1, 2, 1)
 plt.plot(epochs_range, dice, label='Training Dice Loss')
-#plt.plot(epochs_range, val_dice, label='Validation Dice Loss')
+# plt.plot(epochs_range, val_dice, label='Validation Dice Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Dice Loss')
 
 plt.subplot(1, 2, 2)
 plt.plot(epochs_range, loss, label='Training Loss')
-#plt.plot(epochs_range, val_loss, label='Validation Loss')
+# plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 
